374e7e50e3001bece510cfd859efd2d0baa8305f
Filipe Barreto <36456738+BarretoFilipe@users.noreply.github.com>
2023-10-16 12:34:14 +0100

    Update check.go

--- check.go
+++ check.go
@@ -25,9 +25,13 @@ func main() {
 	fmt.Println("Load .env")
 	loadEnv()
 	urls := replaceSplitString(os.Getenv("URLS"), "\n", ",")
+	seconds, errConversion := strconv.Atoi(os.Getenv("SECONDS_TO_CHECK"))
+	if errConversion != nil {
+		port = 300
+	}
 
 	fmt.Println("Start Process")
-	for range time.Tick(time.Second * 10) {
+	for range time.Tick(time.Second * seconds) {
 		fmt.Println("Check Websites - " + time.Now().UTC().Format(DDMMYYYYhhmmss))
 		for _, url := range urls {
 			resp, err := http.Get(url)
